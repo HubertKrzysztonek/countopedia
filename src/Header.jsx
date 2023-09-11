34c579b6454786f5a187548be6e8a2433de8b267
HubertKrzysztonek <hubertkrzysztonekk@gmail.com>
2023-09-11 20:29:31 +0200

    style change + upload images

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,9 +2,9 @@ import logo from './Images/logo192.png';
 
 function Header(){
     return(
-            <div className='pt-3 pl-2'>
+            <div className='py-2 pl-2' style={{ borderBottom: "1px solid #777"}}>
                 <img src={logo} alt="nie dziala" style={{height:"35px",verticalAlign:"top"}} />
-                <span className='h2 pt-4 text-white-50'>React Course - Countopedia</span>
+                <span className='h2 pt-4 m2 text-white-50'>React Course - Countopedia</span>
             </div>
     )
 }
